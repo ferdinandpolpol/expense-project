1110bf6d1cda6d95bf2f3c733c7ddf90981b8880
Ferdinand Polpol <ferdinand.polpol@gmail.com>
2018-05-04 16:01:37 +0800

    Finished Add new Expense, Changeed UI

--- app.js
+++ app.js
@@ -10,13 +10,27 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.set('view engine', 'ejs');
 
 // Array to store expenses data
+// pre-made values for example purposes
 const expenses = [
     { id: 1, description: "Expense 1", amount: 4000.00, date: Date.now()},
     { id: 2, description: "Expense 2", amount: 1890.00, date: Date.now()},
 ];
 
 app.get('/', (req, res) => {
-    console.log(expenses);
+    // Passes the expenses variable to show it to the View
+    res.render('index', {expenses: expenses});
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+    // Stores the HTML Form values to expense variable
+    const expense = {
+        id: expenses.length + 1,
+        description: req.body.description,
+        amount: parseFloat(req.body.amount),
+        date: req.body.date,
+    }
+    expenses.push(expense);
+
     res.render('index', {expenses: expenses});
 });
 
