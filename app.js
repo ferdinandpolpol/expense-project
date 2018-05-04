f902d8df7c1d1689e088e1d89229bca3f482c551
Ferdinand Polpol <ferdinand.polpol@gmail.com>
2018-05-04 14:05:51 +0800

    Node, UI
    1. installed various Node modules such as Express and Nodemon
    2. Added Beauter CSS and JS libraries
    3. Created the basic look of the Website

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(express.json());
 // Array to store expenses data
 const expenses = [];
 
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/views/index.html', 'utf8', (err, data) => {
+        if(err) throw err;
+        res.end(data);
+    });
+});
+
 // Create Web Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port: ${port}...`));
